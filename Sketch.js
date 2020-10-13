a242943be93b9f1d7512c7686882a62e93c6b5db
CoolBeans112 <71298276+CoolBeans112@users.noreply.github.com>
2020-10-12 17:30:41 -0700

    Create Sketch.js

---
+++ Sketch.js
@@ -0,0 +1,6 @@
+function setup() {
+  let clientHeight = document.getElementById('main').clientHeight;
+  let clientWidth = document.getElementById('main').clientWidth;
+  let c = createCanvas(clientWidth,clientHeight );
+  c.parent('main');
+}
