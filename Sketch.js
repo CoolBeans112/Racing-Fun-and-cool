1933183610cfaf727a2888dfa5e38783722c2bd9
CoolBeans112 <71298276+CoolBeans112@users.noreply.github.com>
2020-10-12 17:53:36 -0700

    Update Sketch.js

--- Sketch.js
+++ Sketch.js
@@ -1,6 +1,11 @@
+let player;
 function setup() {
   let clientHeight = document.getElementById('main').clientHeight;
   let clientWidth = document.getElementById('main').clientWidth;
   let c = createCanvas(clientWidth,clientHeight );
   c.parent('main');
+player= new Player();
+}
+function draw() {
+  player.show;
 }
